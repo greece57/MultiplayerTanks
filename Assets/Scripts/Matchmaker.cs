f7a059409170836a00d151a8612726e5bdf1bd3c
Niko <greece93@gmail.com>
2016-02-08 04:03:03 +0200

    Menu Scene and Matchmaking Script for 2 Players

---
+++ Assets/Scripts/Matchmaker.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Matchmaker : MonoBehaviour {
+
+    public static Matchmaker Instance;
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void MakeMatch()
+    {
+        Debug.Log("Start Matchmaking");
+        PhotonNetwork.ConnectUsingSettings("v0.1"); // Connect to Lobby with GameVersion 0.1
+        PhotonNetwork.automaticallySyncScene = true;
+    }
+
+    public void StopMatchmaking()
+    {
+        if (PhotonNetwork.inRoom)
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+        if (PhotonNetwork.insideLobby)
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+        if (PhotonNetwork.connected)
+        {
+            PhotonNetwork.Disconnect();
+        }
+    }
+
+    void OnConnectedToPhoton()
+    {
+        Menu.Instance.UpdateJoiningProgress("Joining Lobby...");
+        if (PhotonNetwork.insideLobby)
+        {
+            Debug.Log("Inside Lobby");
+        }
+        else
+        {
+            Debug.Log("Connected to Photon. Not Inside Lobby");
+            PhotonNetwork.JoinLobby();
+        }
+    }
+
+    void OnJoinedLobby()
+    {
+        Debug.Log("Joining to Random Room");
+        Menu.Instance.UpdateJoiningProgress("Joining Room...");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Can't join random room!");
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { maxPlayers = 2 }, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        if (PhotonNetwork.room.playerCount == 2)
+        {
+            Debug.Log("Room Full");
+            Menu.Instance.StartGame();
+        }
+        else
+        {
+            Debug.Log("Room not Full");
+            Menu.Instance.UpdateJoiningProgress("Waiting for Player to join...");
+        }
+    }
+
+    void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
+    {
+        if (PhotonNetwork.room.playerCount == 2)
+        {
+            Debug.Log("Room Full");
+            Menu.Instance.StartGame();
+        }
+        else
+        {
+            Debug.Log("Room not Full");
+            Menu.Instance.UpdateJoiningProgress("Waiting for Player to join...");
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    private string CurMenu;
+    private string JoinProgress;
+
+    public static Menu Instance;
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+        CurMenu = "Main";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    /*
+     * Public Methods
+     */
+
+    public void UpdateJoiningProgress(string newProgress)
+    {
+        JoinProgress = newProgress;
+    }
+
+    public void StartGame()
+    {
+
+    }
+    
+
+    /*
+     * Private Methods
+     */
+
+    void ToMenu(string menu)
+    {
+        CurMenu = menu;
+    }
+
+    void OnGUI() {
+        if (CurMenu == "Main")
+            Main();
+        if (CurMenu == "Lobby")
+            Lobby();
+        if (CurMenu == "Joining")
+            Joining();
+    }
+
+    private void Main()
+    {
+        if (GUI.Button(new Rect(0, 0, 128, 32), "Start"))
+        {
+            ToMenu("Lobby");
+        }
+
+    }
+
+    private void Lobby()
+    {
+        if (GUI.Button(new Rect(0, 0, 128, 32), "Join Game"))
+        {
+            JoinProgress = "Connecting To Photon...";
+            ToMenu("Joining");
+        }
+        if (GUI.Button(new Rect(0, 33, 128, 32), "Back"))
+        {
+            ToMenu("Main");
+        }
+    }
+
+    private void Joining()
+    {
+        GUI.Label(new Rect(Screen.width / 2 - 64, Screen.height / 2, 256, 32), JoinProgress);
+        
+        // Make Match
+        Matchmaker.Instance.MakeMatch();
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 64, Screen.height / 2 + 33, 128, 32), "Cancel"))
+        {
+            Matchmaker.Instance.StopMatchmaking();
+            ToMenu("Lobby");
+        }
+    }
+}
